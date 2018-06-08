a45280df3ee781ebbce08c00df78d465aaac5ef0
rockdonald2 <39481074+rockdonald2@users.noreply.github.com>
2018-06-08 18:00:56 +0300

    Add files via upload

---
+++ tanulas75/tanulas75/pelda.cpp
@@ -0,0 +1,22 @@
+﻿#include "stdafx.h"
+#include <iostream>
+
+inline int min(int x, int y)
+{
+	return x > y ? y : x;
+}
+
+int main()
+{
+	int x = 5;
+	int y = 2;
+
+	std::cout << min(x, y) << '\n';
+	/*
+	Lényegében, mielőtt megtörténne a funkció lehívás a compiler észreveszi, hogy ez a funkció egy 'inline' funkció,
+		és az egész funkció tartalmát átmásolja ide
+			mintha ezt írtam volna: 5 > 2 ? 2 : 5;
+	*/
+
+	return 0;
+}
