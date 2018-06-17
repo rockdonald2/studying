497c0b0d3ef2ddea8e38ee93fcc20bf09ba302dc
rockdonald2 <39481074+rockdonald2@users.noreply.github.com>
2018-06-17 14:21:34 +0300

    Add files via upload

---
+++ tanulas711/tanulas711/quiz.cpp
@@ -0,0 +1,75 @@
+﻿#include "stdafx.h"
+#include <iostream>
+
+// 1
+/*
+Egy N számnak a faktoriálisa(N!) az összes szám szorzata 1-től a N számíg(0! = 1).
+	Írj egy rekurzív funkciót ennek értelmében. Teszteld az első 7 faktoriálissal.
+*/
+/*
+int faktorialis(int szam)
+{
+	if (szam == 0)
+		return 1;
+	if (szam == 1)
+		return 1;
+	else
+		return faktorialis(szam - 1) * szam;
+}
+
+int main()
+{
+	std::cout << "faktorialis " << faktorialis(5);
+
+	return 0;
+}*/
+
+
+// 2
+/*
+Írj egy rekurzív funkciót amely vesszi egy integer input számait, és különszedve őket összeadja.
+(pl. 357 = 3 + 5 + 7). Teszteld 93427-el aminek 25 a számjegyeinek összege.
+*/
+/*
+int szamjegyekOsszege(int szam)
+{
+	if (szam == 0)
+		return 0;
+	if (szam == 1)
+		return 0;
+	else
+		return szamjegyekOsszege(szam / 10) + szam % 10;
+}
+
+int main()
+{
+	std::cout << szamjegyekOsszege(357);
+
+	return 0;
+}*/
+
+// 3
+/*
+Írj egy rekurzív funkciót ami az int inputot átalakítja binárissá.
+*/
+/*
+void atalakitBinaris(int szam)
+{
+	if (szam == 0)
+		return;
+
+	atalakitBinaris(szam / 2);
+
+	std::cout << szam % 2;
+}
+
+int main()
+{
+	int szam;
+	std::cout << "Irj be egy szamot:";
+	std::cin >> szam;
+
+	atalakitBinaris(szam);
+
+	return 0;
+}*/
