74848cf373675610e4dfd8c09771cea49b921bbd
Lukács Zsolt <39481074+rockdonald2@users.noreply.github.com>
2018-12-18 19:34:41 +0200

    tanulas134

---
+++ tanulas134/tanulas134/pelda.cpp
@@ -0,0 +1,39 @@
+﻿#include "pch.h"
+#include <iostream>
+
+// következő példa egy static array, melyben a template type paramter kezeli az adattípusát a static array-nek,
+	// míg a non-type parameter az array méretét.
+
+template <typename T, int size>
+class StaticArray
+{
+private:
+	T m_array[size];
+
+public:
+	T* getArray();
+
+	T& operator[] (int index)
+	{
+		return m_array[index];
+	}
+};
+
+template <typename T, int size>
+T* StaticArray<T, size>::getArray()
+{
+	return m_array;
+}
+
+int main()
+{
+	StaticArray<int, 12> intArray;
+
+	for (int count = 0; count < 12; ++count)
+		intArray[count] = count;
+
+	for (int count = 11; count >= 0; --count)
+		std::cout << intArray[count] << ' ';
+
+	return 1337;
+}
---
+++ tanulas134/tanulas134/tanulas134.cpp
@@ -0,0 +1,16 @@
+﻿#include "pch.h"
+#include <iostream>
+
+/*
+
+Non-type parameter,
+	a template non-type parameter az egy speciális típusú paraméter amely nem egy típus helyettesítője,
+		hanem ehelyett felcseréli egy értékkel.
+		- egy non-type parameter a következők közül lehet:
+			- egy érték aminek integral típusa van, vagy enum
+			- egy mutató vagy hivatkozás egy class objectre
+			- egy mutató vagy hivatkozás egy funkcióra
+			- egy mutató vagy hivatkozás egy class tag funkcióra
+			- std::nullptr_t
+
+*/
