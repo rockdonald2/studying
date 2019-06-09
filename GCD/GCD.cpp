d207bcc9892d2a84131f8c54d731099614b29822
rockdonald2 <rockdonald2@gmail.com>
2019-06-09 22:20:52 +0300

    20190609

---
+++ GCD/GCD.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+/*
+GCD: greatest common divisor
+
+	1. IF A=0 --> GCD(0, B) = B
+	2. IF B=0 --> GCD(A, 0) = A
+	3. A = B * Q + R
+	4. GCD(A, B) == GCD(B, R)
+	
+*/
+
+int gcd(int a, int b) {
+	int r;
+	// int q;
+
+	if (a == 0)
+		return b;
+	if (b == 0)
+		return a;
+	r = a % b;
+	// q = a / b;
+	// a = b * q + r
+
+	return gcd(b, r);
+}
+
+
+int main(void) {
+
+	int divisor = gcd(0, 49);
+	std::cout << divisor;
+
+	return 0;
+}
