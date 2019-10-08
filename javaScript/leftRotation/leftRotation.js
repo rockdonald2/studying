e83a43d4df91da19a40382c79900e46abf118612
rockdonald2 <rockdonald2@gmail.com>
2019-10-08 15:45:13 +0300

    2019.10.08

---
+++ javaScript/leftRotation/leftRotation.js
@@ -0,0 +1,24 @@
+/* const leftRotate = (array, numberOfRotates) => {
+    while (numberOfRotates !== 0) {
+        const temp = array[0];
+
+        for (let i = 0; i < array.length - 1; i++) {
+            array[i] = array[i + 1];
+        }
+
+        array[array.length - 1] = temp;
+        numberOfRotates--;
+    }
+
+    return array;
+} */
+
+const leftRotate = (array, numberOfRotates) => {
+    let temp = array.splice(0, d);
+    array.splice(array.length, 0, ...temp);
+    return array;
+}
+
+const a = [1, 2, 3, 4, 5];
+const d = 4;
+console.log(leftRotate(a, d));
