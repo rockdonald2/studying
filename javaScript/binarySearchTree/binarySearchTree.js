3614bbeceb8c55f200de5e120304ec4d39d44b55
rockdonald2 <rockdonald2@gmail.com>
2019-10-08 14:46:49 +0300

    2019.10.08

--- javaScript/binarySearchTree/binarySearchTree.js
+++ javaScript/binarySearchTree/binarySearchTree.js
@@ -10,6 +10,7 @@ class BinarySearchTree {
     constructor() {
         this.root = null;
         this.result = [];
+        this.queue = [];
     }
 
     findMin() {
@@ -222,9 +223,181 @@ class BinarySearchTree {
 
         return this.result;
     }
+
+    levelOrder(node = this.root) {
+        if (!this.root) {
+            return null;
+        }
+
+        this.queue.push(node);
+
+        while (this.queue.length !== 0) {
+            let current = this.queue.shift();
+            this.result.push(current.value);
+
+            if (current.left) {
+                this.queue.push(current.left);
+            }
+
+            if (current.right) {
+                this.queue.push(current.right);
+            }
+        }
+
+        return this.result;
+    }
+
+    reverseLevelOrder(node = this.root) {
+        if (!this.root) {
+            return null;
+        }
+
+        this.queue.push(node);
+
+        while (this.queue.length !== 0) {
+            let current = this.queue.shift();
+            this.result.push(current.value);
+
+            if (current.right) {
+                this.queue.push(current.right);
+            }
+
+            if (current.left) {
+                this.queue.push(current.left);
+            }
+        }
+
+        return this.result;
+    }
+
+    remove(value) {
+        let found = false;
+        let parent = null;
+        let current = this.root;
+        let childCount = 0;
+        let replacement;
+        let replacementParent;
+
+        if (this.isPresent(value)) {
+            while (!found && current) {
+                if (value < current.value) {
+                    parent = current;
+                    current = current.left;
+                } else if (value > current.value) {
+                    parent = current;
+                    current = current.right;
+                } else {
+                    found = true;
+                }
+            }
+
+            if (found) {
+                childCount = (current.left === null ? 0 : 1) + (current.right === null ? 0 : 1);
+
+                if (current === this.root) {
+                    switch (childCount) {
+                        case 0: this.root = null; return null;
+
+                        case 1: this.root = (current.right === null ? current.left : current.right); return null;
+
+                        case 2: {
+                            replacement = current.left;
+
+                            if (this.inorder().length === 3) {
+                                replacement.right = current.right;
+
+                                this.root = replacement;
+                                this.result.length = 0;
+                                return null;
+                            }
+
+                            while (replacement.right !== null) {
+                                replacementParent = replacement;
+                                replacement = replacement.right;
+                            }
+
+                            if (replacementParent !== null) {
+                                replacementParent.right = replacement.left;
+
+                                replacement.right = this.root.right;
+                                replacement.left = this.root.left;
+                            } else {
+                                replacement.right = this.root.right;
+                            }
+
+                            this.root = replacement;
+
+                            return null;
+                        }
+                    }
+                } else {
+                    switch (childCount) {
+                        case 0: {
+                            if (current.value < parent.value) {
+                                parent.left = null;
+                            } else if (current.value > parent.value) {
+                                parent.right = null;
+                            }
+
+                            return null;
+                        }
+
+                        case 1: { 
+                            if (current.value < parent.value) {
+                                parent.left = (current.left === null ? current.right : current.left);
+                            } else if (current.value > parent.value) {
+                                parent.right = (current.left === null ? current.right : current.left);
+                            }
+
+                            return null;
+                        }
+
+                        case 2: {
+                            replacement = current.left;
+
+                            while (replacement.right !== null) {
+                                replacementParent = replacement;
+                                replacement = replacement.right;
+                            }
+
+                            replacementParent.right = replacement.left;
+
+                            replacement.right = current.right;
+                            replacement.left = current.left;
+
+                            if (current.value < parent.value) {
+                                parent.left = replacement;
+                            } else {
+                                parent.right = replacement;
+                            }
+
+
+                            return null;
+                        }
+                    }
+                }
+            }
+        } else {
+            return null;
+        }
+    }
+
+    invert(node = this.root) {
+        if (!node) {
+            return null;
+        }
+
+        var right = this.invert(node.right);
+        var left = this.invert(node.left);
+
+        node.left = right;
+        node.right = left;
+
+        return node;
+    }
 }
 
 let test = new BinarySearchTree();
-
-test.add(7); test.add(1); test.add(9); test.add(0); test.add(3); test.add(8); test.add(10); test.add(2); test.add(5); test.add(4); test.add(6);
+test.add(3); test.add(2); test.add(4); test.add(6); test.add(0); test.add(10);
+test.invert();
 console.log(test.inorder());
