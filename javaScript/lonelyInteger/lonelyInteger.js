97c5e3bf25842956c9042fc2ec4950d56bf02992
rockdonald2 <rockdonald2@gmail.com>
2019-12-21 21:00:44 +0200

    2019.12.21

---
+++ javaScript/lonelyInteger/lonelyInteger.js
@@ -0,0 +1,24 @@
+function lonelyinteger(a) {
+    if (a.length == 1) {
+        return a[0];
+    }
+
+    let dict = {};
+    for (let i = 0; i < a.length; i++) {
+        if (a[i] in dict) {
+            dict[a[i]] += 1;
+        } else {
+            dict[a[i]] = 1;
+        }
+    }
+
+    for (let i in dict) {
+        if (dict[i] != 1) {
+            delete dict[i];
+        }
+    }
+
+    return Object.keys(dict)[0];
+}
+
+console.log(lonelyinteger([0, 0, 1, 2, 1]));
